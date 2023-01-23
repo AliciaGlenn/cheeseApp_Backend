7b991ef45e750c72926f10864c947634c35db93a
AliciaGlenn <Alicia@AliciaGlenn.com>
2023-01-23 05:19:08 -0500

    install dependencies express mongoose method-override ejs dotenv morgan, setup scripts, established database connection, built cheese model

---
+++ server.js
@@ -0,0 +1,81 @@
+///////////////////////////////
+// DEPENDENCIES
+////////////////////////////////
+// get .env variables
+require("dotenv").config();
+// pull PORT from .env, give default value of 3000
+const { PORT = 4000, DATABASE_URL } = process.env;
+// import express
+const express = require("express");
+// create application object
+const app = express();
+// import middlware
+const cors = require("cors");
+const morgan = require("morgan");
+
+// import mongoose
+const mongoose = require("mongoose");
+
+mongoose.connect(DATABASE_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+// Connection Events
+mongoose.connection
+  .on("open", () => console.log("Your are connected to mongoose"))
+  .on("close", () => console.log("Your are disconnected from mongoose"))
+  .on("error", (error) => console.log(error));
+
+///////////////////////////////
+// MODELS
+////////////////////////////////
+const CheeseSchema = new mongoose.Schema({
+  name: String,
+  countryOfOrigin: String,
+  image: String,
+});
+
+const Cheese = mongoose.model("Cheese", CheeseSchema);
+
+///////////////////////////////
+// MiddleWare
+////////////////////////////////
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging
+app.use(express.json()); // parse json bodies
+
+///////////////////////////////
+// ROUTES
+////////////////////////////////
+// create a test route
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+// Cheese INDEX ROUTE
+app.get("/cheese", async (req, res) => {
+  try {
+    // send all Cheese
+    res.json(await Cheese.find({}));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
+
+// Cheese CREATE ROUTE
+app.post("/cheese", async (req, res) => {
+  try {
+    // send all people
+    res.json(await Cheese.create(req.body));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
+
+///////////////////////////////
+// LISTENER
+////////////////////////////////
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
